82e88d613dd8e6032a7f9ae5f220c456c40d6ec4
Lizok <lizokgrugorenchuk@gmail.com>
2023-04-25 21:54:03 +0300

    first

---
+++ src/main/java/ua/com/public_utilities/PublicUtilitiesApplication.java
@@ -0,0 +1,13 @@
+package ua.com.public_utilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicUtilitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicUtilitiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/public_utilities/PublicUtilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.public_utilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicUtilitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

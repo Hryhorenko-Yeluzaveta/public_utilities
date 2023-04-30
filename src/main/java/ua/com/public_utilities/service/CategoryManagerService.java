6c79771ebe773d607b4595779358cf8955d363ff
Lizok <lizokgrugorenchuk@gmail.com>
2023-04-30 21:44:35 +0300

    base version

---
+++ src/main/java/ua/com/public_utilities/service/CategoryManagerService.java
@@ -0,0 +1,76 @@
+package ua.com.public_utilities_admin.service;
+
+import org.springframework.stereotype.Service;
+import ua.com.public_utilities_admin.entity.Category;
+import ua.com.public_utilities_admin.repository.CategoryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryManagerService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryManagerService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+
+    public Category saveCategory(String name, String descr, String image){
+
+        Category category = new Category();
+        category.setName(name);
+        category.setDescription(descr);
+        category.setImage(image);
+
+        Category category1 =  categoryRepository.save(category);
+
+        return  category1;
+    }
+
+    public Category updateCategory(Long id, String name, String descr, String image){
+
+        Category category = new Category();
+        category.setId(id);
+        category.setName(name);
+        category.setDescription(descr);
+        category.setImage(image);
+
+        Category category1 =  categoryRepository.save(category);
+
+        return  category1;
+    }
+
+    public void deleteCategory(Long id){
+        categoryRepository.deleteById(id);
+    }
+
+
+    public void deleteAllCategory(){
+        categoryRepository.deleteAll();
+    }
+
+
+    public List<Category> getCategoryList(){
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id){
+
+        Optional<Category> categories = categoryRepository.findById(id);
+
+        if(categories.isEmpty()){
+            return null;
+        } else {
+            return categories.get();
+        }
+    }
+
+    public Category getCategoryByName(String name) {
+
+        return categoryRepository.findByName(name);
+    }
+
+
+}
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,7 @@
+<body>
+
+<h1> Hello Student!!! </h1>
+<a href="/customermanager">User </a>
+
+</body>
+</html>
